436050640eed318122798333977bfa8084b81fe8
Dayana Hoyos Riascos <da.hoyos@duocuc.cl>
2025-04-06 19:11:50 -0400

    agrego diseño homev1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 import { NgxSonnerToaster } from 'ngx-sonner';
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet, NgxSonnerToaster],
+  imports: [IonApp, IonRouterOutlet, NgxSonnerToaster, FormsModule],
 })
 export class AppComponent {
   constructor() {}
--- src/app/views/navbar/home/home.component.ts
+++ src/app/views/navbar/home/home.component.ts
@@ -1,7 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+interface Categoria {
+  id: string;
+  nombre: string;
+  icono?: string;
+}
+
+interface Transaction {
+  id: string;
+  type: 'ingreso' | 'gasto';
+  amount: number;
+  category: Categoria;
+  date: Date;
+}
 
 @Component({
   selector: 'app-home',
+  imports: [CommonModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
@@ -10,5 +27,103 @@ export default class HomeComponent  implements OnInit {
   constructor() { }
 
   ngOnInit() {}
+  isModalOpen: boolean = false;
+  tipoSeleccionado: 'ingreso' | 'gasto' = 'ingreso';
+  monto: number= 0;
+  categoriaSeleccionada: string = '';
+  defaultTipo: 'ingreso' | 'gasto' = 'ingreso';
+  defaultMonto: number = 0;
+  defaultCategoria: string = '';
+  transactions: Transaction[] = [];
+
+  // Categorías de ingresos
+  categoriasIngreso: Categoria[] = [
+    { id: 'salario', nombre: 'Salario' },
+    { id: 'inversiones', nombre: 'Inversiones' },
+    { id: 'ventas', nombre: 'Ventas' },
+    { id: 'regalos', nombre: 'Regalos recibidos' },
+    { id: 'otros_ingresos', nombre: 'Otros ingresos' }
+  ];
+
+  // Categorías de gastos
+  categoriasGasto: Categoria[] = [
+    { id: 'comida', nombre: 'Alimentación' },
+    { id: 'transporte', nombre: 'Transporte' },
+    { id: 'vivienda', nombre: 'Vivienda' },
+    { id: 'entretenimiento', nombre: 'Entretenimiento' },
+    { id: 'servicios', nombre: 'Servicios' },
+    { id: 'salud', nombre: 'Salud' },
+    { id: 'ropa', nombre: 'Ropa y accesorios' },
+    { id: 'educacion', nombre: 'Educación' },
+    { id: 'otros_gastos', nombre: 'Otros gastos' }
+  ];
+
+  resetForm() {
+    this.tipoSeleccionado = this.defaultTipo;
+    this.monto = this.defaultMonto;
+    this.categoriaSeleccionada = this.defaultCategoria;
+  }
+  
+  openModal() {
+    this.isModalOpen = true;
+  }
+  closeModal() {
+    this.isModalOpen = false; 
+    this.resetForm();  
+  }
+
+ 
+
+  guardarTransaccion(): void {
+    if (this.monto <= 0 || this.categoriaSeleccionada === '') {
+      console.error('Por favor, completa todos los campos correctamente.');
+      return;
+    }
+
+    if (this.tipoSeleccionado === 'gasto'){
+      const saldoActual = this.calcularSaldo();
+      if (this.monto > saldoActual){
+        alert('No puedes gastar más de lo que tienes. Tu saldo actual es: ' + saldoActual);
+        return;
+      }
+    }
+
+
+    this.thisTransaction();
+    // Aquí agregarías la lógica para guardar la transacción
+    console.log('Guardando transacción:', {
+      tipo: this.tipoSeleccionado,
+      monto: this.monto,
+      categoria: this.categoriaSeleccionada
+    });
+  }
+
+  thisTransaction(): void {
+    const nuevaTransaccion: Transaction = {
+      id: Date.now().toString(),
+      type: this.tipoSeleccionado,
+      amount: this.monto,
+      category: this.categoriasIngreso.find(c => c.id === this.categoriaSeleccionada) || this.categoriasGasto.find(c => c.id === this.categoriaSeleccionada)!,
+      date: new Date()
+    };
+    this.transactions.push(nuevaTransaccion);
+    this.closeModal();
+  }
+
+  calcularIngresos(): number{
+    return this.transactions.reduce((total, t) =>
+      t.type === "ingreso" ? total + t.amount : total, 0);
+  }
+
+  calcularGastos(): number {
+    return this.transactions.reduce((total, t) =>
+      t.type === "gasto" ? total + t.amount : total, 0);
+  }
+
+
+  calcularSaldo(): number {
+    return this.transactions.reduce((saldo, t) =>
+      t.type === "ingreso" ? saldo + t.amount : saldo - t.amount, 0);
+  }
 
 }
