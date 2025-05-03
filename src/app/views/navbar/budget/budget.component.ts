25921adf33a01e5aed6e91ef7d2c38213b8be8a0
YhonVivas <yohnvivas16@gmail.com>
2025-05-03 17:24:00 -0400

    feature: Se agrega libreria chart.js

--- src/app/views/navbar/budget/budget.component.ts
+++ src/app/views/navbar/budget/budget.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { IonContent } from '@ionic/angular/standalone'
+import { Chart, registerables } from 'chart.js';
+
+
+Chart.register(...registerables);
 @Component({
   selector: 'app-budget',
   templateUrl: './budget.component.html',
@@ -8,8 +12,42 @@ import { IonContent } from '@ionic/angular/standalone'
 })
 export default class BudgetComponent  implements OnInit {
 
+  @ViewChild('chartCanvas') private chartCanvas!: ElementRef;
+  private chart: Chart | undefined;
+
   constructor() { }
 
   ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.createChart();
+  }
+
+  private createChart(): void {
+    const ctx = this.chartCanvas.nativeElement.getContext('2d');
+    
+    this.chart = new Chart(ctx, {
+      type: 'bar', // Puedes cambiar el tipo según necesites
+      data: {
+        labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio'],
+        datasets: [{
+          label: 'Presupuesto 2025',
+          data: [1200, 1900, 3000, 5000, 2200, 3300],
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          borderColor: 'rgba(75, 192, 192, 1)',
+          borderWidth: 1
+        }]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
 
 }
