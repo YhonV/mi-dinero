f184db9afb8d1c0154921cdfe53ae797383e521a
YhonVivas <yohnvivas16@gmail.com>
2025-05-03 17:18:00 -0400

    Commit para sincronizar cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,23 +2,14 @@ import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 import { NgxSonnerToaster } from 'ngx-sonner';
 import { FormsModule } from '@angular/forms';
-import { NgxChartsModule } from '@swimlane/ngx-charts'
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet, NgxSonnerToaster, FormsModule,NgxChartsModule],
+  imports: [IonApp, IonRouterOutlet, NgxSonnerToaster, FormsModule],
 })
 export class AppComponent {
   constructor() {}
-
-  // saleData = [
-  //   { name: "Mobiles", value: 105000 },
-  //   { name: "Laptop", value: 55000 },
-  //   { name: "AC", value: 15000 },
-  //   { name: "Headset", value: 150000 },
-  //   { name: "Fridge", value: 20000 }
-  // ];
 
 }
